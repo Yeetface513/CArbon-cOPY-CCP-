496981eb577521b700a6bc4af170ff09471a2b0a
Yeetface513 <123550198+Yeetface513@users.noreply.github.com>
2023-12-18 14:26:12 +0000

    gpt every time

--- skibiditoilet.c
+++ skibiditoilet.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
-
 // this comment says you should eat your left big toe
 int main() {
+  char sigma[] = "skibidi";
+  double skibidi = "6.6666666666666666";
   printf("Hey guys I have diabetes! \n");
-  printf("\"sugnoma ;)\"\n\n");
+  printf("\"sugnoma ;)\"\n");
+  printf("%s",sigma);
   return 0;
 }
