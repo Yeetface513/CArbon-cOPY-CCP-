b716345b97b2581c6ba36e7c8227568a84e2945a
Yeetface513 <123550198+Yeetface513@users.noreply.github.com>
2023-12-18 11:45:05 +0000

    your so skibidi

--- skibiditoilet.c
+++ skibiditoilet.c
@@ -1,1 +1,8 @@
-#include <studio.h>
+#include <stdio.h>
+
+// this comment says you should eat your left big toe
+int main() {
+  printf("Hey guys I have diabetes! \n");
+  printf("\"sugnoma ;)\"\n\n");
+  return 0;
+}
